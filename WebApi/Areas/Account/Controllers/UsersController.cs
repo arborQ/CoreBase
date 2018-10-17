4096540e2a54ed83d7d540ef0a0a6eb9d1ed233e
Lukasz Wojcik <lwojcik@pgs-soft.com>
2018-10-17 10:53:20 +0200

    add values

---
+++ WebApi/Areas/Account/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Areas.Account.Controllers
+{
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Values()
+        {
+            return new string[] {
+                "1",
+                "2",
+                "3",
+                "test",
+            };
+        }
+    }
+}
